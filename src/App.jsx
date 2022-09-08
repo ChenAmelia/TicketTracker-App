df8461f9e89e7e1c073c9cf620ada3da19aa48f4
Chen <liangchen_amelia@163.com>
2022-09-08 12:36:28 +0100

    counter card set

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./App.scss";
 import Nav from "./Components/Nav/Nav";
 import Counter from "./Components/Counter/Counter";
+//import CounterCard from "./Components/CounterCard/CounterCard";
 
 
 const App = () => {
 
   return (
     <div className="app">
-
       <Nav />
 
       <div className="container">
 
-        <Counter />
+        <div className="counterCard">
+          <heading className="counterCard__heading">Customer</heading>
+          <Counter />
+        </div>
 
-
+        
 
       </div>
 
--- src/Components/CounterCard/CounterCad.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CounterCad = () => {
-  return (
-    <div>CounterCad</div>
-  )
-}
-
-export default CounterCad
